ee9cba9c4324563d1f379ed76e3b144bf3412182
keleris32 <charles.ejiegbu@gmail.com>
2021-09-14 23:14:01 +0100

    feat: Initialized api

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
+require('dotenv').config({ path: './.env' });
+
 const express = require('express');
 const cors = require('cors');
 
-// require('dotenv').config({ path: './.env' });
-
 const port = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+const waistApparelRoute = require('./routes/waistApparelRoute');
+const tightsApparelRoute = require('./routes/tightsApparelRoute');
+const padApparelRoute = require('./routes/padApparelRoute');
 
 // Call fn to connect to MongoDB Atlas cluster
 connectDB();
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors()); // Cross-Origin-Resource-Sharing
+app.use(express.json()); // Parse to JSON
+
+// Routes
+app.use('/api/apparel/waist-trainers', waistApparelRoute); // Waist Apparel route
+app.use('/api/apparel/butt-lifters', tightsApparelRoute); // Tights Apparel route
+app.use('/api/apparel/arm-shapers', padApparelRoute); // Pad Apparel route
 
 app.listen(port, () => {
-  console.log('Server is Initializing!');
-});
-
-app.get('/', (req, res) => {
-  res.send('Touchdown bitches!!!!');
+  console.log(`Server is running on port: ${port}!`);
 });
