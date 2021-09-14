60cd8cebf30ac111f25ee237537ef28d90e49a7b
keleris32 <charles.ejiegbu@gmail.com>
2021-09-14 23:12:06 +0100

    Apparel data added

---
+++ data/products.js
@@ -0,0 +1,87 @@
+const products = [
+  (waist_apparel = [
+    {
+      title: 'Zipped up and Double-Snatched workout waist trainer',
+      description: 'Zipped up and Double-Snatched Waist Trainer',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631317822/ebbie-stores/waist-apparel/6o86Nu8O-removebg-preview_u5wjhz.png',
+      alt: "Ebbie store's Waist Trainer",
+      price: 10000.0,
+    },
+    {
+      title: 'Extreme Power Tummy Belt',
+      description: 'Extreme Power Tummy Belt',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631318207/ebbie-stores/waist-apparel/IMG_3290-removebg-preview_ukyuky.png',
+      alt: "Ebbie store's Extreme Power Belt",
+      price: 8000.0,
+    },
+    {
+      title: 'Hook & Zip Waist Trainer',
+      description: 'Hook & Zip Waist Trainer',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631318271/ebbie-stores/waist-apparel/IMG_7184-removebg-preview_vujuqr.png',
+      alt: "Ebbie store's Hook & Zip Waist Trainer",
+      price: 10000.0,
+    },
+    {
+      title: '25 Steel Bone Corset',
+      description: '25 Steel Bone Corset',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631318340/ebbie-stores/waist-apparel/IMG_7189-removebg-preview_tiehqw.png',
+      alt: "Ebbie store's 25 Steel Bone Corset",
+      price: 15000.0,
+    },
+    {
+      title: 'Detachable Tummy Compressor',
+      description: 'Detachable Tummy Compressor',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631318390/ebbie-stores/waist-apparel/IMG_7183-removebg-preview_yb17xi.png',
+      alt: "Ebbie store's Detachable Tummy Compressor",
+      price: 6000.0,
+    },
+  ]),
+  (tights_apparel = [
+    {
+      title: 'Paded Tummy Control Butt Lifter',
+      description: 'Paded Tummy Control Butt Lifter',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631318641/ebbie-stores/tights-apparel/IMG_3294-removebg-preview_ol1eip.png',
+      alt: "Ebbie store's Paded Tummy Control Butt Lifter",
+      price: 5000.0,
+    },
+    {
+      title: 'Padded Butt underwear',
+      description: 'Padded Butt underwear',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631318690/ebbie-stores/tights-apparel/IMG_3288-removebg-preview_wsng9b.png',
+      alt: "Ebbie store's Padded Butt underwear",
+      price: 5000.0,
+    },
+    {
+      title: 'High Waist Detachable Butt Lifter',
+      description: 'High Waist Detachable Butt Lifter',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631318747/ebbie-stores/tights-apparel/indexWaistTrainer_fkpkzo.png',
+      alt: "Ebbie store's High Waist Detachable Butt Lifter",
+      price: 8500.0,
+    },
+    {
+      title: 'Butt Lifter Tummy Control Underwear',
+      description: 'Butt Lifter Tummy Control Underwear',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631318799/ebbie-stores/tights-apparel/IMG_7188-removebg-preview_lgtvji.png',
+      alt: "Ebbie store's Butt Lifter Tummy Control Underwear",
+      price: 5000.0,
+    },
+    {
+      title: 'Seamless High Waist Butt Lifter Panty',
+      description: 'Seamless High Waist Butt Lifter Panty',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631318846/ebbie-stores/tights-apparel/IMG_6335_kbdesz.png',
+      alt: "Ebbie store's Seamless High Waist Butt Lifter Panty",
+      price: 4000.0,
+    },
+  ]),
+  (pad_apparel = [
+    {
+      title: 'Double-Snatched workout Arm Pads',
+      description: 'Double-Snatched workout Arm Pads',
+      img: 'https://res.cloudinary.com/kel32/image/upload/v1631319046/ebbie-stores/pad-apparel/_DSC2064-removebg-preview_ezilwi.png',
+      alt: "Ebbie store's quality arm pad",
+      price: 7500.0,
+    },
+  ]),
+];
+
+module.exports = products;
